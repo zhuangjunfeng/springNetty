89e06ad2e01b2ce6babd861a703c87f99a712358
鱼西 华 <semstouch@gmail.com>
2016-10-29 20:36:45 +0800

    删除客户端代码

--- src/main/java/com/elin4it/ssm/listener/InitDataListener.java
+++ src/main/java/com/elin4it/ssm/listener/InitDataListener.java
@@ -1,5 +1,7 @@
 package com.elin4it.ssm.listener;
 
+import com.elin4it.ssm.netty.client.ProtocolServer;
+import com.elin4it.ssm.netty.websocket.WebsocketChatServer;
 import com.elin4it.ssm.pojo.Dictionary;
 import com.elin4it.ssm.service.DictionaryService;
 import org.springframework.beans.factory.InitializingBean;
@@ -8,7 +10,11 @@ import org.springframework.web.context.ServletContextAware;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
+
 /**
  * 内容管理平台服务启动监听器
  * 查询字典数据存入缓存
@@ -26,5 +32,21 @@ public class InitDataListener  implements InitializingBean,ServletContextAware{
         System.out.println("-------------------系统初始化字典数据結束--------------");
         // NettyServer  nettyServer = new NettyServer();
         // nettyServer.start(port,dispatcherServlet);
+
+
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("config/netty.properties");
+        Properties p = new Properties();
+        try{
+            p.load(inputStream);
+        } catch (IOException e1){
+            e1.printStackTrace();
+        }
+        try {
+            new WebsocketChatServer(Integer.parseInt(p.getProperty("websocket.port"))).run();
+            new ProtocolServer(Integer.parseInt(p.getProperty("client.port"))).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
