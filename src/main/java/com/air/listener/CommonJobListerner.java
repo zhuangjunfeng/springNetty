fec0ea4e067fe2ef4fe35c55a27d34bd1afadc83
鱼西 华 <semstouch@gmail.com>
2017-01-20 23:16:45 +0800

    spring配置任务监听实时在线用户数和设备数

--- src/main/java/com/air/listener/CommonJobListerner.java
+++ src/main/java/com/air/listener/CommonJobListerner.java
@@ -1,20 +1,48 @@
 package com.air.listener;
 
+import io.netty.channel.Channel;
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
 
+import javax.servlet.ServletContext;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/12/15.
  */
 @Component
-public class CommonJobListerner {
-	
+public class CommonJobListerner implements ApplicationContextAware,ServletContextAware{
+    private static Logger logger = Logger.getLogger(CommonJobListerner.class);
+
+    private ApplicationContext applicationContext;
+    private ServletContext servletContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
+    }
 
     @Scheduled(cron = "0 0/1 * * * ? ")//每隔1秒隔行一次
     public void run(){
+        Map<String,Channel> websocketMap=(Map)servletContext.getAttribute("websocketMap");
+        Map<String,Channel> clientMap=(Map)servletContext.getAttribute("clientMap");
+        logger.info("当前客户端在线人数为："+websocketMap.size());
+        logger.info("当前设备端在线人数为："+clientMap.size());
+    }
 
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext=servletContext;
     }
 }
