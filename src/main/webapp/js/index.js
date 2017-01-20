ac0d13d36794a2c274c3d3e83a8427ee8f8147bb
鱼西 华 <semstouch@gmail.com>
2017-01-20 08:50:14 +0800

    设备新增表单验证

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,10 +1,35 @@
 $(function() {
     'use strict';
 
-    $(".bar-tab a").click(function(){
-        $.router.load("#"+$(this).attr("menu-data"));
+    $(".bar-tab a").click(function() {
+        $.router.load("#" + $(this).attr("menu-data"));
     })
-    $(".button-success").click(function(){
+    $(".button-success").click(function() {
+            var rs = true;
+            if ($("input[name='device_name']").val() == "") {
+                $.alert('请输入设备名称!');
+                rs = false;
+                return;
+            }
+            if ($("input[name='device_uid']").val() == "") {
+                $.alert('请输入设备UID!');
+                rs = false;
+                return;
+            }
+            if (rs) {
+                add();
+            }
+
+        })
+        //设备列表
+    $(document).on("pageInit", "#device-list", function(e, id, page) {
+        query();
+    });
+    $(document).on("pageInit", "#device-add", function(e, id, page) {
+        $('#add-form')[0].reset();
+    });
+
+    function add() {
         $.ajax({
             type: "POST",
             dataType: "json",
@@ -13,32 +38,25 @@ $(function() {
                 "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
             },
             data: $("#add-form").serialize(),
-            success: function (data) {
-
+            success: function(data) {
+                $.router.load("#device-list");
             }
         })
+    }
 
-    })
-    //设备列表
-    $(document).on("pageInit", "#device-list", function(e, id, page) {
-        query();
-    });
-
-    function query(){
+    function query() {
         $.ajax({
-            type:"GET",
-            dataType:"json",
-            url:"/rest/device/find",
-            data:{_method:"GET"},
-            success:function(data) {
-                var typeList=data.data.list;
-                var listHtml="";
-                $.each(typeList,function(i,n) {
-                    listHtml+="<div class='list-block media-list'><ul><li><a href='#device-info' class='item-link item-content'>" +
+            type: "GET",
+            dataType: "json",
+            url: "/rest/device/find",
+            data: { _method: "GET" },
+            success: function(data) {
+                var typeList = data.data.list;
+                var listHtml = "";
+                $.each(typeList, function(i, n) {
+                    listHtml += "<div class='list-block media-list'><ul><li><a href='#device-info' class='item-link item-content'>" +
                         "<div class='item-media'>" +
-                        "<img src='img/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg' style='width: 4rem;'></div>"
-                        +"<div class='item-inner'> <div class='item-title-row'><div class='item-title'>"
-                        + n.device_name+
+                        "<img src='img/TB10LfcHFXXXXXKXpXXXXXXXXXX_!!0-item_pic.jpg_250x250q60.jpg' style='width: 4rem;'></div>" + "<div class='item-inner'> <div class='item-title-row'><div class='item-title'>" + n.device_name +
                         "</div><div class='item-after'>在线</div></div><div class='item-subtitle'>当前环境</div><div class='item-text'>PM超标甲醛超标</div> </div> </a> </li> </ul> </div>"
                 })
 
