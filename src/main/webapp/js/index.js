028fd168ad083b5e50f2dda97eb7d8cfaab4d892
huayuxi <semstouch@gmail.com>
2017-03-17 08:24:36 +0800

    设备绑定和设备解绑

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -111,11 +111,7 @@ function query() {
             var typeList = data.data.list;
             var listHtml = "";
             if(typeList.length==0){
-                listHtml+="<div class='binding-content'><div class='binding-state'>"
-                    +"<span class='icon iconfont icon-xiaolian'></span></div> <div class='binding-p'>"
-                   +"您还没有绑定设备，请先进行绑定 </div><div class='binding-button'><a href='http://air.semsplus.com/rest/wx/binding' class='button button-success'>设备绑定</a>"
-                    +"</div></div>";
-                $("#device-list").html(listHtml);
+                window.location.href = "http://air.semsplus.com/noData.html" ;
             }else {
                 $.each(typeList, function (i, n) {
                     listHtml += "<div class='list-block media-list'><ul><li>" +
