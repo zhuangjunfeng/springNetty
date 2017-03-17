123feef90d89cad16c992fc2cdc501b8aa87cfd2
huayuxi <semstouch@gmail.com>
2017-03-17 09:23:23 +0800

    修复表单bug

--- src/main/webapp/js/binding.js
+++ src/main/webapp/js/binding.js
@@ -2,25 +2,29 @@
  * 添加设备
  */
 function add() {
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "http://air.semsplus.com/rest/device",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-        },
-        data: $("#add-form").serialize(),
-        success: function(data) {
-            if(data.message=="success") {
-                $.alert('绑定成功！');
-                window.location.href = "http://air.semsplus.com/rest/wx/go";
+    if($('obj[name="device_name"]').val()==""||$('obj[name="device_uid"]').val()==""){
+        $.alert('设备名称或UID不能为空！');
+    }else{
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "http://air.semsplus.com/rest/device",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            data: $("#add-form").serialize(),
+            success: function(data) {
+                if(data.message=="success") {
+                    $.alert('绑定成功！');
+                    window.location.href = "http://air.semsplus.com/rest/wx/go";
+                }
+                if(data.message=="isIn") {
+                    $.alert('设备已存在！');
+                }
+                if(data.message=="onLogin") {
+                    window.location.href = "http://air.semsplus.com/rest/wx/go";
+                }
             }
-            if(data.message=="isIn") {
-                $.alert('设备已存在！');
-            }
-            if(data.message=="onLogin") {
-                window.location.href = "http://air.semsplus.com/rest/wx/go";
-            }
-        }
-    })
+        })
+    }
 }
